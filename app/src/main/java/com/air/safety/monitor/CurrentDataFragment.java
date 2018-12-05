9ee7e910a2125fd2c20443a4b7c1d81ffdcf8eb6
Kenneth Chen <kenchen0606@gmail.com>
2018-12-05 15:10:27 -0500

    stop input crash

--- app/src/main/java/com/air/safety/monitor/CurrentDataFragment.java
+++ app/src/main/java/com/air/safety/monitor/CurrentDataFragment.java
@@ -82,7 +82,11 @@ public class CurrentDataFragment extends Fragment{
                 int co = Integer.parseInt(coVal.getText().toString());
 
                 CurrentValue currentValue = new CurrentValue(pm, voc, co2, co);
-                ref.child(id).setValue(currentValue);
+
+                if (pm!=0 && voc!=0 && co2!=0 && co!=0) {
+                    ref.child(id).setValue(currentValue);
+                }
+
             }
         });
     }
